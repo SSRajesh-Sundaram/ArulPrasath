079779cdee2de016ec1979bf575ab6a7352d608f
SSRajesh-Sundaram <74827012+SSRajesh-Sundaram@users.noreply.github.com>
2020-12-27 03:47:26 +0530

    Update Actions01.java

--- Actions01.java
+++ Actions01.java
@@ -1,9 +1,9 @@
 package action;
-
 public class Actions01 {
-
-	public static void main(String[] args) {
-System.out.println("Arul Prasath Video")
-
-	}
+public static void main(String[] args) {
+System.out.println("Arul Prasath Video");
+	
+	System.out.println("Trail and Error");
+	System.out.println("New Branch");
+}
 }
