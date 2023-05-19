7c8603a21a64e1615001446a67acc7e4b8d36c6e
Sandy404646 <130440500+Sandy404646@users.noreply.github.com>
2023-05-19 20:34:05 +0530

    FIle Added
    Practise file added

--- Actions01.java
+++ Actions01.java
@@ -3,7 +3,9 @@ package action;
 public class Actions01 {
 
 	public static void main(String[] args) {
-System.out.println("Arul Prasath Video")
-System.out.println("To Main Branches")
+System.out.println("Arul Prasath Video");
+System.out.println("To Main Branches");
+System.out.println("Santhosh Added a file");
+
 	}
 }
